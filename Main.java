de6a908d9ffc4eafaab12d4408c0d0bfb0fa5ee6
ppPooNnn <pun_yavee@live.com>
2024-04-01 14:36:06 +0700

    delete unnecessary code

--- Main.java
+++ Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
         Menu m = new Menu();
         m.Display();
-
-        // Process m = new Process();
-        // m.run();
-
-        // CreateAutomaton ca = new CreateAutomaton();
-
-        // ca.runSlide19();
     }
 }
